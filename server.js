67056b4f581601e1a68d1c4152952de95e7cf804
Tony Fenoaina <tfenoaina@gmail.com>
2024-03-13 23:13:13 +0300

    changement url

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ let assignment = require('./routes/assignments');
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.set('debug', true);
-
+const uri = 'mongodb+srv://tfenoaina:garnacho2004@cluster0.vi1u5wq.mongodb.net/angularAssignments?retryWrites=true&w=majority&appName=Cluster0'
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
-const uri = 'mongodb+srv://mb1:toto@cluster0.lxvcyxy.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
+// const uri = 'mongodb+srv://mb1:toto@cluster0.lxvcyxy.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
